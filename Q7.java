374b144664a53b558295268a7316a20b7ac50e4c
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:55:44 +0530

    seventh commit

---
+++ Q7.java
@@ -0,0 +1,12 @@
+//Program to check result of given equation:  5%(5*2)-3+6/2 = ? 
+class Q7 
+{
+    public static void main(String args[])
+    {
+        float calc;
+        calc=5%(5*2)-3+6/2;
+        System.out.println("Result of eq. 5%(5*2)-3+6/2 = "+calc);
+    }
+}
+
+
