2fa37e12bcde9069929cc0e73d31c92babe99151
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:45:05 +0530

    second commit

---
+++ Q2.java
@@ -0,0 +1,11 @@
+//Program to take float input and print it.
+public class Q2 
+{
+    public static void main(String[] args) 
+    {
+        float a=6.5f;
+        System.out.println("value of a is :"+a);
+    
+    }
+}
+
