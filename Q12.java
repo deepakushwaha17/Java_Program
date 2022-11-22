7f39c0a139aa778bc3c4315c01f010cfb70cef4a
Deepa <deepakushwaha155@gmail.com>
2022-11-22 22:12:10 +0530

    twelfth commit

---
+++ Q12.java
@@ -0,0 +1,18 @@
+//  Program which enter any integer than calculate and print its square and cube.
+import java.util.*;
+public class Q12 
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter integer number:");
+        int num=sc.nextInt();
+        if(num<5)
+         System.out.println("Cube of "+num+" is "+(num*num*num));
+         else
+         System.out.println("Square of "+num+" is "+(num*num));
+ 
+
+    }
+    
+}
