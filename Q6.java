43b2c4846b0a6800887cdca107749544e864b5d9
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:54:57 +0530

    sixth commit

---
+++ Q6.java
@@ -0,0 +1,10 @@
+//Program to check result of given equation:  8/8 + 7%2 - 3*2 =?
+class Q6
+{
+    public static void main(String args[])
+    {
+        float calc;
+        calc=8/8+7%2-3*2;
+        System.out.println("Result of eq. 8/8+7%2-3*2 = "+calc);
+    }
+}
