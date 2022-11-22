fb98871b76da2489b10675ddc6e1e50331723e74
Deepa <deepakushwaha155@gmail.com>
2022-11-22 22:38:14 +0530

    twenty-first commit

---
+++ Q21.java
@@ -0,0 +1,27 @@
+/*Program which enter sales made by a any salesman that calculate and print the 
+commission, according to following rules -
+ sale up to 25000, commission is 5% of sale, otherwise it is & 7% of sales
+  output format -
+        commission = .......Rs */
+import java.util.*;
+public class Q21
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the sales :");
+        int sale=sc.nextInt();
+        if(sale<=25000)
+        {
+            double commis=(sale*5)/100;
+            System.out.println("Commission ="+commis+"Rs.");
+        }
+        else
+        {
+            double commis=(sale*7)/100;
+            System.out.println("Commission ="+commis+" Rs.");
+        }
+    
+    }
+}
+
