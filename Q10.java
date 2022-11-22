95c0bdeba7813f9a8ae083e27baa51d5e2c7358e
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:59:56 +0530

    tenth commit

---
+++ Q10.java
@@ -0,0 +1,15 @@
+//Program which enter any integer than calculate and print its square.
+import java.util.*;
+class Q10 
+{
+    public static void main(String args[])
+    {
+        int a,sqr;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a integer number:");
+        a=sc.nextInt();
+        sqr=a*a;
+        System.out.println("Square of "+a+" is:"+sqr);
+    }
+    
+}
