42c7c4cdcc0fa6c57f8fa2374702033b36e5ff70
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:49:59 +0530

    third commit

---
+++ Q3.java
@@ -0,0 +1,16 @@
+//Program to take input from user and add two number and print sum .
+import java.util.*;
+class  Q3
+{
+    public static void main(String args[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a and b:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Sum of a and b :"+c);
+    }
+
+}
