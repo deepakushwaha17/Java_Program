b6eab9553033d5c4edc03cb07de054251d2fd290
Deepa <deepakushwaha155@gmail.com>
2022-11-22 22:16:29 +0530

    seventeenth commit

---
+++ Q17.java
@@ -0,0 +1,15 @@
+/*Program which store two values 10 and 20. Then print them after 
+swap(exchange) them without using third variable and print them. */
+class Q17
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("swap of number a=10 and b=20 is : a="+a+" b="+b);
+    }
+}
+
