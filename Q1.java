1a7d89a3334978c01dd643de2136b207ae91ea89
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:42:20 +0530

    first commit

---
+++ Q1.java
@@ -0,0 +1,9 @@
+//Program which store value 65 in computer memories and print it.
+public class Q1 
+{
+    public static void main(String[] args) {
+        int a=65;
+        System.out.println("value of a is :"+a);
+    }
+    
+}
