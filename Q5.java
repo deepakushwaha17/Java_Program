08e07bb1f54728aca9826a78e93c72206f3cccd4
Deepa <deepakushwaha155@gmail.com>
2022-11-22 21:53:20 +0530

    fifth commit

---
+++ Q5.java
@@ -0,0 +1,16 @@
+//Program to input age of a person and if age is 18 or more ,then print you are adult otherwise print you are minor
+import java.util.*;
+public class Q5 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the age of person:");
+        int a=sc.nextInt();
+        if(a>=18)
+         System.out.println("You are Adult");
+        else
+         System.out.println("You are Minor");
+    }
+    
+}
