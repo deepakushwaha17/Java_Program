1a2f64d05e48d11eb0fef03738fa87bfd430c509
Deepa <deepakushwaha155@gmail.com>
2022-11-22 22:17:31 +0530

    eighteenth commit

---
+++ Q18.java
@@ -0,0 +1,16 @@
+//Program which enter any integer then check and print whether it is odd no. or even no.
+import java.util.*;
+public class Q18 
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter integer number:");
+        int num=sc.nextInt();
+        if(num%2==0)
+        System.out.println("Even Number");
+        else
+        System.out.println("Odd Number");
+        
+    }
+}
