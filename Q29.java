6f870ef0f3561a665b1cfafd4e17309f7b98c10f
Deepa <deepakushwaha155@gmail.com>
2022-11-22 22:44:07 +0530

    twenty-ninth commit

---
+++ Q29.java
@@ -0,0 +1,21 @@
+/*program which enter age of a person if the age is 18 or more then print you are adult otherwise
+ print you are minor using Conditional operator [? :] */
+import java.util.*;
+public class Q29
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age :");
+        int age=sc.nextInt();
+        boolean flag;
+        flag=(age>=18)?true:false;
+        //int i;
+        //i=(age>=18)?1:0;
+        if(flag==true)
+        System.out.println("Adult");
+        else
+        System.out.println("Minor");
+        
+    }
+}
