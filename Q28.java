c8bc75658afe8cd9e25313c4238fbaceaf795716
Deepa <114289926+deepakushwaha17@users.noreply.github.com>
2024-01-19 08:33:51 +0530

    Q28.java

--- Q28.java
+++ Q28.java
@@ -1,5 +1,6 @@
 /* program which enter any character then check and print whether it is Capital alphabet, 
  small alphabet, digit or any other special character*/
+
 import java.util.*;
 public class Q28
 {
